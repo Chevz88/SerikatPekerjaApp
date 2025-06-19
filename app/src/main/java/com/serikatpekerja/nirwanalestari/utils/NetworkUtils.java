6e79ac07e1dbd464482ae207e1844628797611b4
Chevz88 <chevz.hanavimichi@gmail.com>
2025-06-19 22:00:39 +0700

    Update and rename fileutils.java to NetworkUtils.java

---
+++ app/src/main/java/com/serikatpekerja/nirwanalestari/utils/NetworkUtils.java
@@ -0,0 +1,16 @@
+package com.serikatpekerja.nirwanalestari.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class NetworkUtils {
+    public static boolean isOnline(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm != null) {
+            NetworkInfo info = cm.getActiveNetworkInfo();
+            return info != null && info.isConnected();
+        }
+        return false;
+    }
+}
