fd5e6b1840cb7c3e188323a581a54e7e9d471206
Chevz88 <chevz.hanavimichi@gmail.com>
2025-06-19 21:59:50 +0700

    Create Constants.java

---
+++ app/src/main/java/com/serikatpekerja/nirwanalestari/utils/Constants.java
@@ -0,0 +1,5 @@
+package com.serikatpekerja.nirwanalestari.utils;
+
+public class Constants {
+    public static final String BASE_URL = "https://script.google.com/macros/s/AKfycbyB6UBbh3LW7QGltFAF3XJGA5YnZefUv38gQ7CG5CQ/";
+}
