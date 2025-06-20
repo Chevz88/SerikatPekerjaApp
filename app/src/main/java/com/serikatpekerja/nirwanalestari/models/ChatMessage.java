03382dd977155963c0d48892752f21bd18109104
Chevz88 <chevz.hanavimichi@gmail.com>
2025-06-20 13:02:43 +0700

    Update ChatMessage.java

--- app/src/main/java/com/serikatpekerja/nirwanalestari/models/ChatMessage.java
+++ app/src/main/java/com/serikatpekerja/nirwanalestari/models/ChatMessage.java
@@ -2,6 +2,18 @@ package com.serikatpekerja.nirwanalestari.models;
 
 public class ChatMessage {
     private String text;
-    public ChatMessage(String text) { this.text = text; }
-    public String getText() { return text; }
+    private boolean isUser;
+
+    public ChatMessage(String text, boolean isUser) {
+        this.text = text;
+        this.isUser = isUser;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public boolean isUser() {
+        return isUser;
+    }
 }
