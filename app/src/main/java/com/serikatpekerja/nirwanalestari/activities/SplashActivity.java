b79d9a0ec8f6834b9408cd2fce157850ac74321c
Chevz88 <chevz.hanavimichi@gmail.com>
2025-06-20 10:57:18 +0700

    Update SplashActivity.java

--- app/src/main/java/com/serikatpekerja/nirwanalestari/activities/SplashActivity.java
+++ app/src/main/java/com/serikatpekerja/nirwanalestari/activities/SplashActivity.java
@@ -1,14 +1,3 @@
-// File: app/src/main/java/com/serikatpekerja/nirwanalestari/activities/SplashActivity.java
-package com.serikatpekerja.nirwanalestari.activities;
-
-import android.content.Intent;
-import android.media.MediaPlayer;
-import android.net.Uri;
-import android.os.Bundle;
-import android.widget.VideoView;
-import androidx.appcompat.app.AppCompatActivity;
-import com.serikatpekerja.nirwanalestari.R;
-
 public class SplashActivity extends AppCompatActivity {
 
     @Override
@@ -16,25 +5,15 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        VideoView videoView = findViewById(R.id.videoViewSplash);
-        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.video; // Ganti "video" dengan nama file Anda
-        Uri uri = Uri.parse(videoPath);
-        videoView.setVideoURI(uri);
+        VideoView splashVideo = findViewById(R.id.splashVideo);
+        Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.video_splash_compressed);
+        splashVideo.setVideoURI(videoUri);
 
-        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            public void onCompletion(MediaPlayer mp) {
-                startNextActivity();
-            }
+        splashVideo.setOnCompletionListener(mp -> {
+            startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            finish();
         });
 
-        videoView.start();
-    }
-
-    private void startNextActivity() {
-        if (isFinishing())
-            return;
-        // Langsung ke halaman Login setelah video selesai
-        startActivity(new Intent(SplashActivity.this, LoginActivity.class));
-        finish();
+        splashVideo.start();
     }
 }
