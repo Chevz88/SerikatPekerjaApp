91d28cd7bbbe742ea7dd326255f78fba27e1fff2
Chevz88 <chevz.hanavimichi@gmail.com>
2025-06-20 11:45:41 +0700

    Create WebviewActivity.java

---
+++ app/src/main/java/com/serikatpekerja/nirwanalestari/activities/WebviewActivity.java
@@ -0,0 +1,31 @@
+package com.serikatpekerja.nirwanalestari.activities;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.serikatpekerja.nirwanalestari.R;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+        webView = findViewById(R.id.webView);
+
+        // Ambil URL dari intent
+        String url = getIntent().getStringExtra("url");
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
+    }
+}
