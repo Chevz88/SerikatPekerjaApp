a07d2d18e2c9498553e9d0c525c9fce6febfa9da
Chevz88 <chevz.hanavimichi@gmail.com>
2025-06-20 12:55:25 +0700

    Update and rename WebviewActivity.java to WebViewActivity.java

--- app/src/main/java/com/serikatpekerja/nirwanalestari/activities/WebViewActivity.java
+++ app/src/main/java/com/serikatpekerja/nirwanalestari/activities/WebViewActivity.java
@@ -1,29 +1,20 @@
 package com.serikatpekerja.nirwanalestari.activities;
 
 import android.os.Bundle;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.serikatpekerja.nirwanalestari.R;
 
 public class WebViewActivity extends AppCompatActivity {
-
-    WebView webView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webview);
 
-        webView = findViewById(R.id.webView);
-        String url = getIntent().getStringExtra("url");
-
-        WebSettings settings = webView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl(url);
+        WebView webview = findViewById(R.id.webview);
+        webview.setWebViewClient(new WebViewClient());
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl("https://kemnaker.go.id/");
     }
 }
