c490873f927969c122834718477adffb2ef5cbb4
Chevz88 <chevz.hanavimichi@gmail.com>
2025-06-20 14:25:38 +0700

    Update SplashActivity.java

--- app/src/main/java/com/serikatpekerja/nirwanalestari/activities/SplashActivity.java
+++ app/src/main/java/com/serikatpekerja/nirwanalestari/activities/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.serikatpekerja.nirwanalestari.activities;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.VideoView;
@@ -8,18 +9,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.serikatpekerja.nirwanalestari.R;
 
 public class SplashActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
         VideoView splashVideo = findViewById(R.id.splashVideo);
-        Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.video_splash_compressed);
+        Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.video_splash);
         splashVideo.setVideoURI(videoUri);
+
         splashVideo.setOnCompletionListener(mp -> {
             startActivity(new Intent(SplashActivity.this, LoginActivity.class));
             finish();
         });
+
         splashVideo.start();
     }
 }
