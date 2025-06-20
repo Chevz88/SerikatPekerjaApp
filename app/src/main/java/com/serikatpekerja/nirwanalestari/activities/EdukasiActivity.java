ecedcd035b5c421ec69484fbba7412a914b592df
Chevz88 <chevz.hanavimichi@gmail.com>
2025-06-20 13:21:41 +0700

    Update EdukasiActivity.java

--- app/src/main/java/com/serikatpekerja/nirwanalestari/activities/EdukasiActivity.java
+++ app/src/main/java/com/serikatpekerja/nirwanalestari/activities/EdukasiActivity.java
@@ -3,22 +3,38 @@ package com.serikatpekerja.nirwanalestari.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.serikatpekerja.nirwanalestari.R;
 
 public class EdukasiActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edukasi);
+    }
 
-        Button btnLihat = findViewById(R.id.btnLihat);
-        btnLihat.setOnClickListener(v -> {
-            Intent intent = new Intent(this, EdukasiDetailActivity.class);
-            startActivity(intent);
-        });
+    public void openHakPekerja(View view) {
+        Intent intent = new Intent(this, EdukasiDetailActivity.class);
+        intent.putExtra("judul", "Hak-Hak Pekerja");
+        intent.putExtra("isi", "Pekerja memiliki hak atas:\n" +
+                "- Upah yang layak\n" +
+                "- Jam kerja sesuai aturan\n" +
+                "- Cuti dan waktu istirahat\n" +
+                "- Keselamatan dan kesehatan kerja (K3)\n" +
+                "- Perlindungan terhadap diskriminasi\n" +
+                "- Hak untuk membentuk dan bergabung dalam serikat pekerja");
+        startActivity(intent);
+    }
+
+    public void openSerikatPekerja(View view) {
+        Intent intent = new Intent(this, EdukasiDetailActivity.class);
+        intent.putExtra("judul", "Apa Itu Serikat Pekerja?");
+        intent.putExtra("isi", "Serikat pekerja adalah organisasi yang dibentuk oleh dan untuk pekerja guna memperjuangkan hak dan kepentingan mereka, seperti:\n" +
+                "- Negosiasi upah\n" +
+                "- Perlindungan hukum\n" +
+                "- Fasilitasi pengaduan\n" +
+                "- Advokasi di tempat kerja");
+        startActivity(intent);
     }
 }
