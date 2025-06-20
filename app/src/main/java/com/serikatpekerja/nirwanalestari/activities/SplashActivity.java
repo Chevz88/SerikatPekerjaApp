36aaf75aa7743050c6fb6d6f7fcc95d698c85646
Chevz88 <chevz.hanavimichi@gmail.com>
2025-06-20 14:35:19 +0700

    Update SplashActivity.java

--- app/src/main/java/com/serikatpekerja/nirwanalestari/activities/SplashActivity.java
+++ app/src/main/java/com/serikatpekerja/nirwanalestari/activities/SplashActivity.java
@@ -1,14 +1,16 @@
 package com.serikatpekerja.nirwanalestari.activities;
 
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.VideoView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.serikatpekerja.nirwanalestari.R;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DURATION = 6000; // 6 detik
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +18,14 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         VideoView splashVideo = findViewById(R.id.splashVideo);
-        Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.video_splash);
+        Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.video_splash_compressed);
         splashVideo.setVideoURI(videoUri);
-
-        splashVideo.setOnCompletionListener(mp -> {
-            startActivity(new Intent(SplashActivity.this, LoginActivity.class));
-            finish();
-        });
-
         splashVideo.start();
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_DURATION);
     }
 }
