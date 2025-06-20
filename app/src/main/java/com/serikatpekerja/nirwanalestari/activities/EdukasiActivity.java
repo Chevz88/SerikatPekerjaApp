9d90734bf987bc6dc4bff293c9764b8816993de2
Chevz88 <chevz.hanavimichi@gmail.com>
2025-06-20 12:57:40 +0700

    Update EdukasiActivity.java

--- app/src/main/java/com/serikatpekerja/nirwanalestari/activities/EdukasiActivity.java
+++ app/src/main/java/com/serikatpekerja/nirwanalestari/activities/EdukasiActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.serikatpekerja.nirwanalestari.R;
 
 public class EdukasiActivity extends AppCompatActivity {
@@ -18,22 +20,5 @@ public class EdukasiActivity extends AppCompatActivity {
             Intent intent = new Intent(this, EdukasiDetailActivity.class);
             startActivity(intent);
         });
-    }
-}
-
-// =======================
-// EdukasiDetailActivity.java
-// =======================
-package com.serikatpekerja.nirwanalestari.activities;
-
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import com.serikatpekerja.nirwanalestari.R;
-
-public class EdukasiDetailActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edukasi_detail);
     }
 }
