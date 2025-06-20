fc8e56566a491ac249effba01ccf9d9886034ab6
Chevz88 <chevz.hanavimichi@gmail.com>
2025-06-20 11:28:19 +0700

    Create EdukasiDetailAcrifity.java

---
+++ app/src/main/java/com/serikatpekerja/nirwanalestari/activities/EdukasiDetailAcrifity.java
@@ -0,0 +1,30 @@
+package com.serikatpekerja.nirwanalestari.activities;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.serikatpekerja.nirwanalestari.R;
+
+public class EdukasiDetailActivity extends AppCompatActivity {
+
+    TextView txtJudul, txtIsi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edukasi_detail);
+
+        txtJudul = findViewById(R.id.txtJudul);
+        txtIsi = findViewById(R.id.txtIsi);
+
+        // Ambil data dari Intent
+        String judul = getIntent().getStringExtra("judul");
+        String isi = getIntent().getStringExtra("isi");
+
+        // Tampilkan
+        txtJudul.setText(judul);
+        txtIsi.setText(isi);
+    }
+}
