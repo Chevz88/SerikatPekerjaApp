23ff99fb94b0a57beb7962aba6a005e29be8b816
Chevz88 <chevz.hanavimichi@gmail.com>
2025-06-20 12:52:55 +0700

    Update EdukasiActivity.java

--- app/src/main/java/com/serikatpekerja/nirwanalestari/activities/EdukasiActivity.java
+++ app/src/main/java/com/serikatpekerja/nirwanalestari/activities/EdukasiActivity.java
@@ -3,32 +3,20 @@ package com.serikatpekerja.nirwanalestari.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.serikatpekerja.nirwanalestari.R;
 
 public class EdukasiActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edukasi);
-    }
 
-    // Saat user klik topik "Hak-Hak Pekerja"
-    public void openHakPekerja(View view) {
-        Intent intent = new Intent(this, EdukasiDetailActivity.class);
-        intent.putExtra("judul", "Hak-Hak Pekerja");
-        intent.putExtra("isi", "Setiap pekerja memiliki hak atas upah yang layak, perlindungan kerja, jaminan sosial, dan hak berserikat sesuai UU No. 13 Tahun 2003 tentang Ketenagakerjaan.");
-        startActivity(intent);
-    }
-
-    // Saat user klik topik "Apa Itu Serikat Pekerja"
-    public void openSerikatPekerja(View view) {
-        Intent intent = new Intent(this, EdukasiDetailActivity.class);
-        intent.putExtra("judul", "Apa Itu Serikat Pekerja?");
-        intent.putExtra("isi", "Serikat pekerja adalah organisasi yang dibentuk dari, oleh, dan untuk pekerja guna memperjuangkan hak dan kepentingan mereka, termasuk negosiasi upah, kondisi kerja, dan keadilan.");
-        startActivity(intent);
+        Button btnLihat = findViewById(R.id.btnLihat);
+        btnLihat.setOnClickListener(v -> {
+            Intent intent = new Intent(this, EdukasiDetailActivity.class);
+            startActivity(intent);
+        });
     }
 }
